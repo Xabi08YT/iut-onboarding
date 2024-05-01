c8df17d1fb6ed292599c11e28208c00c451ad7bd
Xabi GOÏTY <xabigoity@gmail.com>
2024-05-01 16:06:14 +0200

    Started dashboard

--- src/AdminDashboard/views/Main.vue
+++ src/AdminDashboard/views/Main.vue
@@ -1,18 +1,39 @@
 <template>
     <div class="view-content" v-show="isActive">
       <TopBar />
+      <Home
+        v-if="Object.keys(views).includes('home')"
+        :showing="currentView == 'home'"
+      />
     </div>
 </template>
 
 <script>
 import TopBar from "./TopBar.vue";
+import Home from "./Home.vue";
 
 export default {
+  data() {
+    return {
+      currentView: "home",
+      views: {
+        home: {
+          allowed: () => true
+        }
+      }
+    };
+  },
+  methods: {
+    show(view) {
+      this.currentView = view;
+    }
+  },
   props: {
     isActive: Boolean,
   },
   components: {
     TopBar,
+    Home,
   }
 };
 </script>

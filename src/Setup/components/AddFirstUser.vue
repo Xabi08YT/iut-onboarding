48c1b3889457c64262e527754edf5af6c55ea5c3
Xabi GOÏTY <xabigoity@gmail.com>
2024-05-05 15:16:24 +0200

    Finished /setup frontend

--- src/Setup/components/AddFirstUser.vue
+++ src/Setup/components/AddFirstUser.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="setupContent" v-show="isActive">
-        <h1>Hello World</h1>
+        <h1>Ajoutez le premier utilisateur à la base de données</h1>
+        <form id="firstUserSetup">
+          <label for="firstIDNUM"></label>
+          <input id="firstIDNUM" type="text" placeholder="Nom d'utilisateur IDNUM">
+          <input id="firstEmail" type="email" placeholder="Email du CAS">
+          <button type="submit">Ajouter</button>
+        </form>
     </div>
 </template>
 <script>
@@ -10,3 +16,40 @@ export default {
   }
 };
 </script>
+<style>
+#firstUserSetup {
+  display: flex;
+  flex-direction: column;
+  margin-top: 25px;
+}
+
+#firstUserSetup label {
+  margin-top: 25px;
+  margin-left: 15px;
+}
+
+#firstUserSetup input {
+  margin-top: 10px;
+  border-radius: 21px;
+  border: 1px black solid;
+  font-size: 20px;
+  padding: 15px;
+  margin-top: 25px;
+}
+
+#firstUserSetup button {
+  margin-top: 35px;
+  padding: 15px;
+  border-radius: 21px;
+  border: none;
+  background-color: #04c3ff;
+  color: #FFF;
+  font-size: 25px;
+  transition: 0.5s;
+}
+
+#firstUserSetup button:hover {
+  background-color: #01a1CC;
+  transition: 0.5s;
+}
+</style>
--- src/Setup/components/CreatingDB.vue
+++ src/Setup/components/CreatingDB.vue
@@ -1,14 +1,46 @@
 <template>
     <div class="setupContent" v-show="isActive">
         <h1>Construction de la base de données initiale...</h1>
+        <div class="loader"></div>
     </div>
 </template>
 <script>
 export default {
   props: {
     isActive: Boolean
-  }
+  },
+  mounted() {
+    console.log("hello world");
+  },
 };
 </script>
 <style>
+.loader {
+  width: 150px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 8px solid #514b82;
+  animation:
+    l20-1 0.8s infinite linear alternate,
+    l20-2 1.6s infinite linear;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  margin-left: 35%;
+}
+@keyframes l20-1{
+   0%    {clip-path: polygon(50% 50%,0       0,  50%   0%,  50%    0%, 50%    0%, 50%    0%, 50%    0% )}
+   12.5% {clip-path: polygon(50% 50%,0       0,  50%   0%,  100%   0%, 100%   0%, 100%   0%, 100%   0% )}
+   25%   {clip-path: polygon(50% 50%,0       0,  50%   0%,  100%   0%, 100% 100%, 100% 100%, 100% 100% )}
+   50%   {clip-path: polygon(50% 50%,0       0,  50%   0%,  100%   0%, 100% 100%, 50%  100%, 0%   100% )}
+   62.5% {clip-path: polygon(50% 50%,100%    0, 100%   0%,  100%   0%, 100% 100%, 50%  100%, 0%   100% )}
+   75%   {clip-path: polygon(50% 50%,100% 100%, 100% 100%,  100% 100%, 100% 100%, 50%  100%, 0%   100% )}
+   100%  {clip-path: polygon(50% 50%,50%  100%,  50% 100%,   50% 100%,  50% 100%, 50%  100%, 0%   100% )}
+}
+@keyframes l20-2{
+  0%    {transform:scaleY(1)  rotate(0deg)}
+  49.99%{transform:scaleY(1)  rotate(135deg)}
+  50%   {transform:scaleY(-1) rotate(0deg)}
+  100%  {transform:scaleY(-1) rotate(-135deg)}
+}
 </style>

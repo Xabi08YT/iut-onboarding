eb25f4808a632d6751ef80250465980d05dd7f06
Xabi GOÏTY <xabigoity@gmail.com>
2025-02-02 21:00:44 +0100

    Tried to fix container bootloop

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@ if [ ! -f ./.setupfinished ]; then
   npx prisma generate
   node createFirstUser.js
   touch .setupfinished
+  node .output/server/index.mjs
   exit
 fi;
 
 echo "Setup already ran in this environment. If you want to run it again, just delete the .setupfinished file and run this script again."
+echo "Starting..."
+node .output/server/index.mjs
 exit

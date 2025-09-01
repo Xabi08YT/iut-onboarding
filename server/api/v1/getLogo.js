de457f68ddac9c8084e231a48eb4f5f04d5ae232
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-09-01 16:19:20 +0200

    Fixed logos

--- server/api/v1/getLogo.js
+++ server/api/v1/getLogo.js
@@ -4,16 +4,16 @@ export default defineEventHandler(async (event) => {
         let month = new Date().getMonth().toString();
         switch (month) {
             case "1":
-                logo = "/assets/logo_iut_nouvelan_NOTEXT.png";
+                logo = "assets/logo_iut_nouvelan_NOTEXT.png";
                 break;
             case "9":
-                logo = "/assets/logo_iut_halloween.png";
+                logo = "assets/logo_iut_halloween.png";
                 break;
             case "11":
-                logo = "/assets/logo_iut_noel.png";
+                logo = "assets/logo_iut_noel.png";
                 break;
             default:
-                logo = "/assets/logo_iut.png";
+                logo = "assets/logo_iut.png";
         }
         return new Response(logo)
     }

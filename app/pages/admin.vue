c6212656cb572d22d84ea17555ae208873960b27
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-10-08 11:32:09 +0200

    Corrected v-if placement for better look

--- app/pages/admin.vue
+++ app/pages/admin.vue
@@ -47,8 +47,8 @@ init();
 </script>
 
 <template>
-  <div class="flex justify-center m-2">
-    <Button v-if="fullaccess" class="px-4 py-2 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition duration-200" @click="goToCulture()">Culture Panel</Button>
+  <div v-if="fullaccess" class="flex justify-center m-2">
+    <Button class="px-4 py-2 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition duration-200" @click="goToCulture()">Culture Panel</Button>
   </div>
   <div id="container"
        class="w-screen min-h-screen lg:h-dvh lg:max-h-dvh flex flex-col lg:flex-row p-[25px] justify-center items-center">
--- app/pages/culturepanel.vue
+++ app/pages/culturepanel.vue
@@ -32,8 +32,8 @@ init();
 </script>
 
 <template>
-  <div class="flex justify-center m-2">
-    <Button v-if="admin" class="px-4 py-2 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition duration-200" @click="goToAdmin()">Admin Panel</Button>
+  <div v-if="admin" class="flex justify-center m-2">
+    <Button class="px-4 py-2 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition duration-200" @click="goToAdmin()">Admin Panel</Button>
   </div>
   <div id="culturepanel" class="w-screen h-screen flex justify-center items-center">
     <div class="w-1/2 h-[90%]">

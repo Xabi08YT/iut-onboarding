b6027a21b0734349082db9d0e6a15eee40b370ba
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-11-04 09:43:03 +0100

    API Endpoint

--- server/api/v1/hyperplanningEndpoint.js
+++ server/api/v1/hyperplanningEndpoint.js
@@ -8,17 +8,17 @@ export default defineEventHandler(async (event) => {
             let hp_version = await getConfigValue("HPVersion");
             let hp_icals = await getConfigValue("HPIcals");
 
-            return new Response(JSON.stringify({version: hp_version, icals: hp_icals}),{status: 200});
+            return new Response(JSON.stringify({version: hp_version, icals: hp_icals}), {status: 200});
         case "PUT":
             let token = parseCookies(req)?.onboardingToken
-            if(await verifyToken(token) === false) {
-                return new Response(JSON.stringify({message:"Invalid token"}), {status: 401});
+            if (await verifyToken(token) === false) {
+                return new Response(JSON.stringify({message: "Invalid token"}), {status: 401});
             }
 
             let roles = await getRole(token);
 
-            if(roles === -1 || !(roles.includes("ADMIN") || roles.includes("BDE") || roles.includes("MAINTAINER") || !roles.includes("ENSEIGNANT"))) {
-                return new Response(JSON.stringify({message:"Permission denied."}), {status: 403});
+            if (roles === -1 || !(roles.includes("ADMIN") || roles.includes("BDE") || roles.includes("MAINTAINER") || !roles.includes("ENSEIGNANT"))) {
+                return new Response(JSON.stringify({message: "Permission denied."}), {status: 403});
             }
 
             let body = readBody(event);
@@ -27,10 +27,9 @@ export default defineEventHandler(async (event) => {
             try {
                 await updateConfigValue({key: "HPVersion", value: JSON.stringify(parsed.version)});
                 await updateConfigValue({key: "HPIcals", value: JSON.stringify(parsed.icals)})
-                return new Response(JSON.stringify({message:"Success"}), {status: 200});
+                return new Response(JSON.stringify({message: "Success"}), {status: 200});
             } catch (error) {
-                return new Response(JSON.stringify({message:"Internal server error: Request could not be executed. The request may be invalid or the server encountered a problem."}), {status: 500});
+                return new Response(JSON.stringify({message: "Internal server error: Request could not be executed. The request may be invalid or the server encountered a problem."}), {status: 500});
             }
     }
-
 });

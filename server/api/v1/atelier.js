44b46a463882ffa23e0aa8c23112d9d7bfd0c094
Alexandre <alexandre.sans@etu.u-bordeaux.fr>
2025-12-02 17:26:38 +0100

    preventive commit

--- server/api/v1/atelier.js
+++ server/api/v1/atelier.js
@@ -2,6 +2,117 @@ import fs from "fs";
 import {getRole, verifyToken} from "~~/server/jwt";
 import {updateAtelier,createAtelier,getAtelier,deleteAtelier} from "~~/server/database"
 
+
+/**
+ * @openapi
+ * /jpo
+ *      get:
+ *          tags:
+ *              JPO activity management 
+ *          security:
+ *              - JWT: []
+ *          description: get a list containing all activities stored in DB
+ *          responses:
+ *              200:
+ *                  description: get a list containing all activities stored in DB
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object    
+ *                              properties:
+ *                                  id: 
+ *                                      type: integer
+ *                                      description: "id of the activity"
+ *                                      example: 1
+ *                                  name:
+ *                                      type: string
+ *                                      description: "name of teacher holding the activity"
+ *                                      example: "Stéphane FOSSE"
+ *                                  room:
+ *                                      type: string
+ *                                      description: "name of the room where the activity will take place"
+ *                                      example: "204"
+ *                                  start:
+ *                                      type: string
+ *                                      format: datetime
+ *                                      description: "date and hour when the activity will start"
+ *                                      example: "2025-12-02T09:10:00.000Z"
+ *                                  end:
+ *                                      type: string
+ *                                      format: datetime
+ *                                      description: "date and hour when the activity will end"
+ *                                      example: "2025-12-02T19:20:00.000Z"
+ *              500:
+ *                  description : unknown error
+ *      put:
+ *          tags:
+ *              JPO activity management
+ *          security:
+ *              - JWT: []
+ *          request-body:
+ *              required: true
+ *          parameters:
+ *              -in:query
+ *              name: "id"
+ *              required: false
+ *              type: integer
+ *              description: id of the activity
+ *              example: 1
+ *              -in:query
+ *              name:"name"
+ *              required: false
+ *              type: string
+ *              description: "name of teacher holding the activity"
+ *              example: "Stéphane FOSSE"
+ *              -in:query
+ *              name:"start"
+ *              required: false
+ *              type:string
+ *              format:date-time
+ *              description: "date and hour when the activity will start"
+ *              example:"2025-12-02T09:10:00.000Z"
+ *              -in:query
+ *              name:"end"
+ *              required: false
+ *              type:string
+ *              format:date-time
+ *              description: "date and hour when the activity will end"
+ *              example:"2025-12-02T19:20:00.000Z"
+ *      post:
+ *          tags:
+ *              JPO activity management
+ *          security:
+ *              - JWT: []
+ *          request-body:
+ *              required: true
+ *          parameters:
+ *              -in:query
+ *              name: "id"
+ *              required: true
+ *              type: integer
+ *              description: id of the activity
+ *              example: 1
+ *              -in:query
+ *              name:"name"
+ *              required: true
+ *              type: string
+ *              description: "name of teacher holding the activity"
+ *              example: "Stéphane FOSSE"
+ *              -in:query
+ *              name:"start"
+ *              required: true
+ *              type:string
+ *              format:date-time
+ *              description: "date and hour when the activity will start"
+ *              example:"2025-12-02T09:10:00.000Z"
+ *              -in:query
+ *              name:"end"
+ *              required: true
+ *              type:string
+ *              format:date-time
+ *              description: "date and hour when the activity will end"
+ *              example:"2025-12-02T19:20:00.000Z"
+ */
 async function handler(req) {
     let body;
     if (req.method === "GET") {

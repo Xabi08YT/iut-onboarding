7f01558e0e4841eec328dcf0f77d482f4acf5b7a
nilsdx <nils.durieux@gmail.com>
2025-12-03 21:41:30 +0100

    fixed admin panel not showing everything as admin

--- app/pages/admin.vue
+++ app/pages/admin.vue
@@ -5,7 +5,6 @@ import EventEditor from "~~/views/admin/eventEditor.vue";
 import UserEditor from "~~/views/admin/userEditor.vue";
 import DiscordEditor from "~~/views/admin/discordEditor.vue";
 import { navigateTo } from "nuxt/app";
-import {Button} from "~/components/ui/button";
 import AdminTopBar from "../components/AdminTopBar.vue";
 
 const runtimeConfig = useRuntimeConfig();
@@ -32,7 +31,7 @@ const init = async () => {
     bde.value = true;
   }
   if (roles.includes("ADMIN") || roles.includes("MAINTAINER")) {
-    admin.value = true;
+    fullaccess.value = true;
   } else if (!(roles.includes("BDE") || roles.includes("ENSEIGNANT")) && roles.includes("CULTURE")) {
     nuxtApp.runWithContext(() => {
       navigateTo('/culturepanel');
@@ -49,18 +48,6 @@ const init = async () => {
   }
 };
 
-const goToCulture = () => {
-  return navigateTo("/culturepanel");
-};
-
-const goToHP = () => {
-  return navigateTo("/hp");
-};
-
-const goToJPO = () => {
-  return navigateTo("/jpo");
-};
-
 init();
 
 </script>
@@ -70,10 +57,10 @@ init();
   <div id="container"
        class="w-screen min-h-screen lg:h-dvh lg:max-h-dvh flex flex-col lg:flex-row p-[25px] justify-center items-center">
     <Toaster/>
-    <slideEditor class="my-[25px] mx-0 min-w-1/2 min-h-[400px] lg:my-0 lg:mr-[40px] lg:w-1/2 lg:h-full" v-if="admin === true" />
+    <slideEditor class="my-[25px] mx-0 min-w-1/2 min-h-[400px] lg:my-0 lg:mr-[40px] lg:w-1/2 lg:h-full" v-if="fullaccess === true" />
     <div class="flex flex-col justify-center items-center lg:w-1/2 h-full my-0 py-0" id="adminvert">
       <eventEditor class="mx-0 lg:mb-[25px] min-w-full min-h-[500px] lg:min-h-0  lg:h-1/2 mb-[25px]" />
-      <userEditor class="mx-0 min-w-full min-h-[500px] lg:min-h-0 lg:h-1/2" v-if="admin === true" />
+      <userEditor class="mx-0 min-w-full min-h-[500px] lg:min-h-0 lg:h-1/2" v-if="fullaccess === true" />
       <discordEditor class="mx-0 min-w-full min-h-[500px] lg:min-h-0 lg:h-1/2" v-if="bde === true" />
     </div>
   </div>
